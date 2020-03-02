9401bd6a5aaecbd6f1637695420e80644ea17ee7
Hironsan <light.tree.1.13@gmail.com>
2020-03-02 14:59:21 +0900

    Update label serializer to support a recursive relationship

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -56,7 +56,7 @@ class LabelSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Label
-        fields = ('id', 'text', 'prefix_key', 'suffix_key', 'background_color', 'text_color')
+        fields = ('id', 'text', 'prefix_key', 'suffix_key', 'background_color', 'text_color', 'children', 'parent')
 
 
 class DocumentSerializer(serializers.ModelSerializer):
